de24233f72bb5c9bcde3e1af8fc312df04994a6b
Joao Santos <jfs.csantos@gmail.com>
2016-09-29 21:29:21 +0100

    Small bug

--- src/AppBundle/Repository/LotteryRepository.php
+++ src/AppBundle/Repository/LotteryRepository.php
@@ -12,7 +12,7 @@ class LotteryRepository extends \Doctrine\ORM\EntityRepository
   public function findEndedLotteries()
   {
     return $this->getEntityManager()
-      ->createQuery('SELECT lot FROM AppBundle:Lottery lot WHERE lot.endDate < CURRENT_DATE() AND lot.ended = 0 AND lot.winner = NULL ')
+      ->createQuery('SELECT lot FROM AppBundle:Lottery lot WHERE lot.endDate < CURRENT_DATE() AND lot.ended = 0 ')
       ->getResult();
 
   }
